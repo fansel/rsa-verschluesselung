aa3d4b1fe7d824669a5d2cfa6eb17247a1ea81c4
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2023-05-30 19:30:43 +0200

    scanner

--- src/de/jafe/src/Main.java
+++ src/de/jafe/src/Main.java
@@ -1,16 +1,31 @@
 package de.jafe.src;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
         Schluesselgenerator sg = new Schluesselgenerator(35158139,93786151);
         sg.generateKeyPair();
         Encoder en = new Encoder(sg);
-        en.encodeText();
-        en.encode();
         Decoder dc = new Decoder(sg);
-        dc.printDecoded(en.getCharValues());
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file path: ");
+        String filePath = scanner.nextLine();
+        en.readFile(filePath);
 
+        System.out.println("Do you want to encode or decode? (e/d): ");
+        String choice = scanner.nextLine();
+
+        if (choice.equals("e")) {
+            en.encode();
+            System.out.println("Encoded text: ");
+            en.printCharValues();
+        } else if (choice.equals("d")) {
+            System.out.println("Decoded text: ");
+            dc.printDecoded(en.getCharValues());
+        } else {
+            System.out.println("Invalid choice.");
+        }
     }
 }
